00f302ddef71baaf97ca2595ddf5529cf64b03aa
灵 <3185774357@qq.com>
2025-03-16 17:51:51 +1100

    feat: 开启评论插件配置为使用Giscus

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -147,13 +147,13 @@ export default hopeTheme({
   plugins: {
     // Note: This is for testing ONLY!
     // You MUST generate and use your own comment service in production.
-    // comment: {
-    //   provider: "Giscus",
-    //   repo: "vuepress-theme-hope/giscus-discussions",
-    //   repoId: "R_kgDOG_Pt2A",
-    //   category: "Announcements",
-    //   categoryId: "DIC_kwDOG_Pt2M4COD69",
-    // },
+    comment: {
+      provider: "Giscus",
+      repo: "LINKLang/docs",
+      repoId: "R_kgDOOIig-Q",
+      category: "Announcements",
+      categoryId: "DIC_kwDOOIig-c4CoF4c",
+    },
 
     components: {
       components: ["Badge", "VPCard"],
